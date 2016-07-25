5e8f1a12046d0900498237a52462aa9724864bd4
Kelly Lauren Summer Becker-Neuding <kbecker@kellybecker.me>
2016-07-24 17:01:09 -0700

    Updated two `euid != uid` checks with `uid_eq()`

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1429,7 +1429,7 @@ static int cpuset_allow_attach(struct cgroup *cgrp,
 		tcred = __task_cred(task);
 
 		if ((current != task) && !capable(CAP_SYS_ADMIN) &&
-		    cred->euid != tcred->uid && cred->euid != tcred->suid)
+		    !uid_eq(cred->euid, tcred->uid) && !uid_eq(cred->euid, tcred->suid))
 			return -EACCES;
 	}
 
